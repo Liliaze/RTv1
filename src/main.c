6530e148450497cdbad833e92ab017467d185f90
Diana BOUDY <dboudy@e3r8p1.42.fr>
2016-05-18 19:09:37 +0200

    don't forget the main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dboudy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 18:29:58 by dboudy            #+#    #+#             */
-/*   Updated: 2016/05/13 11:36:02 by dboudy           ###   ########.fr       */
+/*   Updated: 2016/05/18 18:55:23 by dboudy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,28 @@ inline static void	init_window_and_img(t_win *win, t_img *img, char *title)
 	img->height = WINH;
 	if ((win->mlx = mlx_init()) == NULL)
 		ft_display_error("mlx_init() does not function");
-	if ((win->win = mlx_new_window(win->mlx, win->wwidth, win->wheight,
-				win->title_win)) == NULL)
+	if ((win->win = mlx_new_window(win->mlx, win->width, win->height,
+				win->title)) == NULL)
 		ft_display_error("mlx_new_window() does not function");
-	if (!(img->id = mlx_new_image(win->mlx, img->iwidth, img->iheight)))
+	if (!(img->id = mlx_new_image(win->mlx, img->width, img->height)))
 		ft_display_error("mlx_new_image() does not function");
 	if (!(img->image = mlx_get_data_addr(img->id, &img->bpp,
 					&img->size_line, &img->endian)))
 		ft_display_error("mlx_get_data_addr() does not function");
 	img->bpp = (img->bpp / 8) / 4;
 	img->size_line = img->size_line / 4;
-	img->last_pixel = img->iheight * img->size_line;
+	img->last_pixel = img->height * img->size_line;
 }
 
 inline static void	init_struct(t_all *all)
 {
 	all->awin = (t_win *)ft_memalloc(sizeof(t_win));
-	all->aimage = (t_img *)ft_memalloc(sizeof(t_img));
+	all->aimg = (t_img *)ft_memalloc(sizeof(t_img));
 	all->acam = (t_cam *)ft_memalloc(sizeof(t_cam));
 	all->aray = (t_ray *)ft_memalloc(sizeof(t_ray));
 	all->av3d = (t_v3d *)ft_memalloc(sizeof(t_v3d));
-	all->aobj = NULL;;
+	all->aobj = (t_obj *)ft_memalloc(sizeof(t_obj));
+	all->acur = (t_obj *)ft_memalloc(sizeof(t_obj));
 }
 
 int	main(int ac, char **av)
@@ -56,10 +57,10 @@ int	main(int ac, char **av)
 	{
 		all = (t_all *)ft_memalloc(sizeof(t_all));
 		init_struct(all);
-		init_window_and_img(all->awin, all->aimage, av[1]);
-		init_scene(all->aobj, av[1]));// lire et parser les infos de la scene ici.
-		// initiliaser les eventuels variables d'en en csq.
-		ray_tracing(all);
+		init_window_and_img(all->awin, all->aimg, av[1]);
+		init_scene(all->acam, all->aobj, &(all->nb_object), av[1]);// lire et parser les infos de la scene ici.
+		ray_tracing(all); // test rendu
+		ft_putstr("tampon");
 		ft_loop(all); /* appel de la boucle event */
 	}
 	return (0);
